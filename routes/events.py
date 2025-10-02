a1263663f041ecd861b839ef0452e548c4acfb3b
Livingstone Asabahebwa <lasabahebwa@gmail.com>
2025-10-02 15:26:00 +0300

    add routes to create and delete events

--- routes/events.py
+++ routes/events.py
@@ -20,3 +20,27 @@ async def retrieve_event(id: int) -> Event:
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Event with supplied ID does not exist",
     )
+
+
+@event_router.post("/new")
+async def create_event(body: Event = Body(...)) -> dict:
+    events.append(body)
+    return {"message": "Event created successfully"}
+
+
+@event_router.delete("/{id}")
+async def delete_event(id: int) -> dict:
+    for event in events:
+        if event.id == id:
+            events.remove(event)
+            return {"message": "Event deleted successfully"}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Event with supplied ID does not exist",
+    )
+
+
+@event_router.delete("/")
+async def delete_all_events() -> dict:
+    events.clear()
+    return {"message": "Events deleted successfully"}
